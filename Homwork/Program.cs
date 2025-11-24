98f9133cebdb8f5e719acdbc6cd48d10dbaf189e
kimin896-sudo <kimin896@gmail.com>
2025-11-24 17:23:35 +0900

    홀수짝수

---
+++ Homwork/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Homwork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine($"{i} 짝수");
+                else
+                    Console.WriteLine($"{i} 홀수");
+            }
+        }
+    }
+}
