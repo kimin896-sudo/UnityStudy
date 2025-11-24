8607d07ebb245085e1574f0ae56422219e08d508
kimin896-sudo <kimin896@gmail.com>
2025-11-24 17:25:45 +0900

    Update Program.cs

--- Homwork/Program.cs
+++ Homwork/Program.cs
@@ -7,9 +7,9 @@
             for (int i = 0; i <= 10; i++)
             {
                 if (i % 2 == 0)
-                    Console.WriteLine($"{i} 짝수");
+                    Console.WriteLine($"{i}는 짝수 입니다.");
                 else
-                    Console.WriteLine($"{i} 홀수");
+                    Console.WriteLine($"{i}은 홀수 입니다.");
             }
         }
     }
