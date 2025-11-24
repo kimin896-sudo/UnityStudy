be2d89e3b7ca48de8fb64d6243b4e42193287385
kimin896-sudo <kimin896@gmail.com>
2025-11-24 17:39:52 +0900

    Update Program.cs

--- StarTree/Program.cs
+++ StarTree/Program.cs
@@ -25,7 +25,41 @@
                 Console.WriteLine();
             }
 
-        
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int k = 0; k <= i; k++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < 5; j++)
+                {
+                    if (j >= i)
+                    {
+                        Console.Write("*");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            // 별만들기1
+            for (int i = 0; i < 5; i++)
+            {
+
+                for (int k = i; k < 5; k++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < 5; j++)
+                {
+                    if (i >= j)
+                        Console.Write("*");
+                }
+
+                
+                Console.WriteLine();
+            }
+            
         }
     }
 }
