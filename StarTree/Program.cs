ef029bfbc93beb6b8edcefb3d0c0dc2be043dd55
kimin896-sudo <kimin896@gmail.com>
2025-11-24 16:35:52 +0900

    Update Program.cs

--- StarTree/Program.cs
+++ StarTree/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+
+                    if (i >= j)
+                        Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (j >= i)
+                        Console.Write("*");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
