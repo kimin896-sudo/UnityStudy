cfa5c186abad095ecab8fcd5c92e7d776bac5807
kimin896-sudo <kimin896@gmail.com>
2025-11-24 15:46:17 +0900

    init2

---
+++ ForMon/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace ForMon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            while(num<= 10)
+            {
+                Console.WriteLine(num);
+                num++;
+            }
+
+        }
+    }
+}
