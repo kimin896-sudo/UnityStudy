f4d693e05ae3508cfc4e406e4ac252f8ce17d2c9
kimin896-sudo <kimin896@gmail.com>
2025-11-24 17:27:35 +0900

    Update Program.cs

--- StarTree/Program.cs
+++ StarTree/Program.cs
@@ -1,9 +1,20 @@
 ﻿namespace StarTree
 {
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            // 별만들기2
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (j >= i)
+                        Console.Write("*");
+                }
+                Console.WriteLine();
+            }
             // 별만들기1
             for (int i = 0; i < 5; i++)
             {
@@ -15,16 +26,7 @@
                 Console.WriteLine();
             }
 
-            // 별만들기2
-            for (int i = 0; i < 5; i++)
-            {
-                for (int j = 0; j < 5; j++)
-                {
-                    if (j >= i)
-                        Console.Write("*");
-                }
-                Console.WriteLine();
-            }
+        
         }
     }
 }
