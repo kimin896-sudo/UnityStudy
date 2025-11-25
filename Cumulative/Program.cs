323d9bde467fbe0b660d1477ed58ea8776a24dea
kimin896-sudo <kimin896@gmail.com>
2025-11-25 09:19:06 +0900

    누적합

---
+++ Cumulative/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Cumulative
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                result += i;
+            }
+            Console.WriteLine(result);
+
+        }
+    }
+}
