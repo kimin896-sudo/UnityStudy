d81d79c7ae5b6cb00c9948f2c3f48705fe8369f5
kimin896-sudo <kimin896@gmail.com>
2025-11-25 10:41:19 +0900

    메소드 Test

--- Cumulative/Program.cs
+++ Cumulative/Program.cs
@@ -2,15 +2,18 @@
 {
     internal class Program
     {
+      
         static void Main(string[] args)
         {
+            
             int result = 0;
             for (int i = 1; i <= 10; i++)
             {
                 result += i;
             }
-            Console.WriteLine(result);
-
+            Console.WriteLine($"{result}");
         }
+
+
     }
 }
---
+++ Method/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Method
+{
+    internal class Program
+    {
+        static int Test(int num1)
+        {
+            num1++;
+            return num1;
+        }
+
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine($"{Test(10)}");
+        }
+    }
+}
